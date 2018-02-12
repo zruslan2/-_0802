ec41bd5e6ee655ac81d4f6fe0a38f2c39bebc39e
ЗемлихановР <@227_2>
2018-02-12 11:45:07 +0600

    .

--- ConsoleApplication9/MyFuncr.h
+++ ConsoleApplication9/MyFuncr.h
@@ -6,14 +6,29 @@ void sort_d(int*arr, int r);
 void sort(int*arr, int r,int n=1);
 
 int numb(type_number tn, int start = 1, int end = 10);
+double numb(type_number tn, double start, int end);
+
 void creat(int*arr, int r, int start, int end, type_number tn);
+#define colCount 5
+void creatMatrix(int(*a)[colCount], int r, int start, int end, type_number tn);
+void creatMatrix(double(*a)[colCount], int r, double start, int end, type_number tn);
+
 
 void print_arr(int*arr, int r);
+void printMatrix(double(*a)[colCount], int r);
+void printMatrix(int(*a)[colCount], int r);
+
 
 void arr_d(int*arr, int arr1[][4], int r);
 
 int sum(int num, ...);
+void Smooth(int *a, int r, const int *k);
+int sum_ind(int(*a)[4], int r, int &col, int &row, int &max);
+double sr_arif(double(*a)[colCount], int r);
+void creatMatrix(int(*a)[colCount], int r, int start, int end, type_number tn);
 
-void smooth(int*a, int r, const int* k);
+
+
+
 
 
--- ConsoleApplication9/func.cpp
+++ ConsoleApplication9/func.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <iostream>
 #include <time.h>
 #include <math.h>
@@ -71,12 +72,50 @@ int numb(type_number tn, int start, int end)
 		break;
 	}
 }
+double numb(type_number tn, double start, int end)
+{
+	switch (tn)
+	{
+	case positive:
+		return (start + rand() % end)/(start + rand() % end);
+		break;
+	case negative:
+		return (start*(-1) + rand() % end)/ (start*(-1) + rand() % end);
+		break;
+	case all:
+		return rand();
+		break;
+	}
+}
 
 void creat(int*arr, int r, int start, int end, type_number tn)
 {
 	for (i = 0; i < r; i++)
 	{
 		arr[i] = numb(tn, start, end);
+	}
+}
+#define colCount 5
+void creatMatrix(int(*a)[colCount], int r, int start, int end, type_number tn)
+{
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < colCount; j++)
+		{
+			a[i][j] = numb(tn, start, end);
+		}
+	}
+}
+
+void creatMatrix(double(*a)[colCount], int r, double start, int end, type_number tn)
+{
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < colCount; j++)
+		{
+			a[i][j] = round(numb(tn, start, end)*100)/100;
+			/*round(a[i][j] = numb(tn, start, end));*/
+		}
 	}
 }
 
@@ -87,6 +126,29 @@ void print_arr(int*arr, int r)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+
+void printMatrix(double(*a)[colCount], int r)
+{
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < colCount; j++)
+		{
+			cout << a[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+void printMatrix(int (*a)[colCount], int r)
+{
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < colCount; j++)
+		{
+			cout << a[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
 
 void arr_d(int*arr, int arr1[][4], int r)
@@ -115,27 +177,61 @@ int sum(int num, ...)
 	va_end(r);
 	return arr;
 }
+//2.	ќписать процедуру Smooth(A,N), замен€ющую каждый элемент вещественного 
+//массива A размера N на его среднее арифметическое со своими сосед€ми
+//("сглаживание массива"). ћассив A Ч входной и выходной параметр,
+//N Ч входной параметр. — помощью этой процедуры выполнить п€тикратное 
+//сглаживание данного массива A размера N, вывод€ на экран результаты каждого сглаживани€.
 
-//1.	ќписать процедуру Smooth(A, N), замен€ющую каждый элемент вещественного массива A размера N на его среднее арифметическое 
-//со своими сосед€ми("сглаживание массива").ћассив A Ч входной и выходной параметр, N Ч входной параметр.— помощью этой процедуры 
-//выполнить п€тикратное сглаживание данного массива A размера N, вывод€ на экран результаты каждого сглаживани€.
-//
-//¬вести с клавиатуры массив, состо€щий из n строк и m столбцов.—оздать библиотеку, в которой будет содержатьс€ функци€, котора€ 
-//производит вычислени€ согласно заданию, приведенному в таблице.
-
-void smooth(int*a, int r, const int* k)
-{	
-	for (int y = 0; y < *k; y++)
+void Smooth(int *a, int r, const int *k)
+{
+	for (int l = 0; l < *k; l++)
 	{
-		a++;
+		a++; //увеличиваем кол-во бит
 		for (j = 1; j < r - 1; j++)
 		{
 			/*a[j] = (a[j - 1] + a[j] + a[j + 1]) / 3;*/
+
 			*a = (*(a - 1) + *a + *(a + 1)) / 3;
 			a++;
 		}
-		a=a - 9;
+		a = a - 9;
 		print_arr(a, r);
 	}
 }
 
+
+int sum_ind(int(*a)[4], int r, int &col, int &row, int &max) //функци€ возвращает более 1 значени€
+{
+	int s;
+	max = *(*a);
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (a[i][j] > max)
+			{
+				max = a[i][j];
+				col = j;
+				row = i;
+				s = i + j;
+			}
+		}
+	}
+	return s;
+}
+
+double sr_arif(double(*a)[colCount], int r)
+{
+	float s = 0,sr;
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+		{
+			s += a[i][j];
+			//s += *(*a);
+			//a++;
+		}
+	}
+	return round((s / (colCount * r))*100)/100;
+}
